f67edb78aaeb690702b44e19313c468103ccebcd
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-02-07 19:35:19 -0500

    Change unsaved icon to a circle

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -27,14 +27,17 @@
       @click.stop="emit('delete')"
       class="ml-3 w-4 h-4 hover:bg-orange-400 text-lg p-0.5 hover:text-black rounded-full text-center group"
     >
-      <PencilIcon class="w-2 h-2" :class="{ 'block group-hover:hidden': marked, 'hidden': !marked }" />
+      <span
+        class="w-1.5 h-1.5 block rounded-full bg-white mx-auto"
+        :class="{ 'block group-hover:hidden': marked, 'hidden': !marked }"
+      />
       <XMarkIcon class="w-3 h-3" :class="{ 'hidden group-hover:block': marked }" />
     </button>
   </button>
 </template>
 
 <script setup lang="ts">
-import { XMarkIcon, PencilIcon } from '@heroicons/vue/20/solid'
+import { XMarkIcon } from '@heroicons/vue/20/solid'
 
 const props = withDefaults(defineProps<{
   title: string,
