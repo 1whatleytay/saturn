35533e27b3cd678e2e73f846254b87824c7339e0
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-02-26 03:10:49 -0500

    Merge consume forwards/backwards into consumeDirection

--- src/utils/query/alt-consume.ts
+++ src/utils/query/alt-consume.ts
@@ -2,36 +2,34 @@ const hardString = ':;<>,./?\'\"[]{}-=+()\`~'
 
 const hardCharacters = new Set(hardString.split(''))
 
-export function isHard(character: string, hard: Set<string>): boolean {
+function isHard(character: string, hard: Set<string>): boolean {
   return hard.has(character) || /\s/.test(character)
 }
 
-export function consumeBackwards(line: string, index: number, hard: Set<string> = hardCharacters): number {
-  if (index <= 0) {
+export function consumeDirection(line: string, index: number, direction: number, hard: Set<string> = hardCharacters): number {
+  const directionOffset = direction < 0 ? 1 : 0
+  const cursor = index - directionOffset
+
+  if (cursor <= 0 || cursor >= line.length) {
     return 1
   }
 
   let result = 0
-  const first = isHard(line[index - 1], hard)
+  const first = isHard(line[cursor], hard)
 
-  while (index - result > 0 && first === isHard(line[index - result - 1], hard)) {
-    result += 1
+  while (cursor + result > 0 && cursor + result < line.length
+    && first === isHard(line[cursor + result], hard)) {
+    result += direction
   }
 
-  return Math.max(1, result)
+  console.log(direction * result)
+  return Math.max(1, direction * result)
+}
+
+export function consumeBackwards(line: string, index: number, hard: Set<string> = hardCharacters): number {
+  return consumeDirection(line, index, -1, hard)
 }
 
 export function consumeForwards(line: string, index: number, hard: Set<string> = hardCharacters): number {
-  if (index >= line.length) {
-    return 1
-  }
-
-  let result = 0
-  const first = isHard(line[index], hard)
-
-  while (index + result < line.length && first === isHard(line[index + result], hard)) {
-    result += 1
-  }
-
-  return Math.max(1, result)
+  return consumeDirection(line, index, +1, hard)
 }
