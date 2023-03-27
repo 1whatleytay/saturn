8da31467338ffc3dd70e1159252c080e0d1f4e1f
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-03-27 01:21:07 -0400

    Allow the use of Ctrl for text jumping

--- src/utils/query/shortcut-key.ts
+++ src/utils/query/shortcut-key.ts
@@ -1,8 +1,8 @@
 import { UAParser } from 'ua-parser-js'
 
-function metaKeyPlatform(): boolean {
-  const os = new UAParser(navigator.userAgent).getOS()
+const os = new UAParser(navigator.userAgent).getOS()
 
+function metaKeyPlatform(): boolean {
   switch (os.name) {
     case 'Mac OS': return true
     default: return false
@@ -16,6 +16,15 @@ interface ActionMarkedEvent {
   ctrlKey: boolean
 }
 
+interface AltMarkedEvent {
+  altKey: boolean
+  ctrlKey: boolean
+}
+
 export function hasActionKey(event: ActionMarkedEvent): boolean {
   return isMetaKey ? event.metaKey : event.ctrlKey
+}
+
+export function hasAltKey(event: AltMarkedEvent): boolean {
+  return event.altKey || (isMetaKey && event.ctrlKey)
 }
