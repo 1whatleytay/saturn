260b36caedcec1313c3d3fc9aa86308ebdd61135
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-06-19 12:38:18 -0700

    Show breakpoints on hover

--- src/components/EditorBody.vue
+++ src/components/EditorBody.vue
@@ -17,11 +17,11 @@
         v-for="i in renderCount"
         :key="getIndex(i)"
         @click="toggleBreakpoint(getIndex(i))"
-        class="w-full h-6 text-right flex items-center justify-end cursor-pointer pointer-events-auto"
+        class="w-full h-6 text-right flex items-center justify-end cursor-pointer pointer-events-auto group"
       >
         <div
-          v-if="hasBreakpoint(getIndex(i))"
-          class="rounded-full bg-red-700 w-3 h-3 mr-auto ml-3"
+          class="rounded-full bg-red-700 w-3 h-3 mr-auto ml-3 opacity-0 group-hover:opacity-30"
+          :class="{'opacity-100 group-hover:opacity-100': hasBreakpoint(getIndex(i))}"
         />
 
         {{ getIndex(i) + 1 }}
