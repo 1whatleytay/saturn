834bf36b0b38957e2bafadbad06a413cb283c6c9
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-07-31 01:13:47 -0700

    Add instruction decoding to debug page

---
+++ src-tauri/src/decode.rs
@@ -0,0 +1,38 @@
+use serde::Serialize;
+use titan::unit::instruction::{InstructionDecoder, InstructionParameter};
+use num::ToPrimitive;
+
+#[derive(Serialize)]
+#[serde(tag="type", content="value")]
+pub enum ParameterItem {
+    Register(u32),
+    Immediate(u16),
+    Address(u32)
+}
+
+#[derive(Serialize)]
+pub struct InstructionDetails {
+    name: &'static str,
+    parameters: Vec<ParameterItem>
+}
+
+#[tauri::command]
+pub fn decode_instruction(pc: u32, instruction: u32) -> Option<InstructionDetails> {
+    let Some(instruction) = InstructionDecoder::decode(pc, instruction) else {
+        return None
+    };
+
+    Some(InstructionDetails {
+        name: instruction.name(),
+        parameters: instruction.registers()
+            .into_iter()
+            .map(|x| {
+                match x {
+                    InstructionParameter::Register(name) => ParameterItem::Register(name.to_u32().unwrap()),
+                    InstructionParameter::Immediate(imm) => ParameterItem::Immediate(imm),
+                    InstructionParameter::Address(address) => ParameterItem::Address(address),
+                }
+            })
+            .collect(),
+    })
+}
