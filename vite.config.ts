e7c9435dcaa0c282eaa02321887483b5b453ebb0
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-11-01 01:48:29 -0400

    Remove TAURI_ from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ['VITE_'],
   build: {
     // Tauri supports es2021
     target: ['es2021', 'chrome100', 'safari13'],
