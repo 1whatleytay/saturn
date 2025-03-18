893fe4ebf2d98cfb570cd244ca1428bdb626c3c2
Milo Mighdoll <14153763+milomg@users.noreply.github.com>
2025-03-18 11:16:02 -0400

    Merge pull request #68 from 1whatleytay/bugfix
    bugfixes

--- src/components/EditorBody.vue
+++ src/components/EditorBody.vue
@@ -74,9 +74,7 @@ onMounted(() => {
   if (/AppleWebKit\/([\d.]+)/.exec(navigator.userAgent)) {
     view.contentDOM.addEventListener(
       'blur',
-      (e): void => {
-        if (!e.relatedTarget) return
-
+      (): void => {
         var editableFix = document.createElement('input')
         editableFix.setAttribute(
           'style',
